a84ba4f0f9b447c964efbf6c1b6aeed72efcf661
STRhenz <rhenzganotice28@gmail.com>
2025-01-06 15:37:33 +0800

    rhhenz pogi

--- View/Login.cs
+++ View/Login.cs
@@ -27,6 +27,7 @@ namespace FitnessConsoleApp.View
                 Console.WriteLine("1. Login");
                 Console.WriteLine("2. Register");
                 Console.WriteLine("3. Exit na to");
+                //RHENZ POGI
                 userInput = Convert.ToInt32(Console.ReadLine());
                 switch (userInput) 
                 {
